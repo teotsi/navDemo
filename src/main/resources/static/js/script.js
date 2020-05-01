2a7313c3012e064e09f7f5027e70562e990ee069
teotsi <teotsi@gmail.com>
2020-05-01 23:54:44 +0300

    frontend work

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -6,8 +6,6 @@ let source = document.querySelector("#source");
 let dstMenu = document.querySelector("#dst-menu");
 let destination = document.querySelector("#destination");
 
-let instructionsContainer = document.querySelector(".instructions-container");
-
 function createOptions(locations, menu, label) {
     locations.forEach((l) => {
         let newLocationA = document.createElement("a");
@@ -47,18 +45,19 @@ function submitForm(event) {
     })
         .then(function (response) {
             let data = response.data;
-            instructionsContainer.textContent = '';
-            instructionsContainer.style.display = "block";
+            let container = document.createElement("div");
+            container.className = "main-container";
+            container.style.gridRow = 5;
             let h3 = document.createElement("h3");
             h3.innerText = "Details";
-            instructionsContainer.appendChild(h3);
+            container.appendChild(h3);
             let detailParagraph = document.createElement("p");
             detailParagraph.innerText = `Distance(m): ${data.distance}, time(ms): ${data.time}`;
-            instructionsContainer.appendChild(detailParagraph);
+            container.appendChild(detailParagraph);
 
             let instH3 = document.createElement("h3");
             instH3.innerText = "Instructions";
-            instructionsContainer.appendChild(instH3);
+            container.appendChild(instH3);
 
             data.instructions.forEach(instruction => {
                 let h4 = document.createElement("h4");
@@ -71,10 +70,9 @@ function submitForm(event) {
                                        time(ms): ${instruction.time}`;
                 container.appendChild(h4);
                 container.appendChild(paragraph);
-
             });
             let grid = document.querySelector(".grid-wrapper");
-            grid.appendChild(instructionsContainer);
+            grid.appendChild(container);
             console.log(response);
         })
         .catch(function (error) {
