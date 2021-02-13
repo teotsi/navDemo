b42b883b315acf13858ec84298e85b3648007f52
teotsi <teotsi@gmail.com>
2021-02-13 17:28:25 +0200

    add swagger docs

--- src/main/java/com/example/demo/Nav/Nav.java
+++ src/main/java/com/example/demo/Nav/Nav.java
@@ -11,38 +11,39 @@ import lombok.*;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Nav {
 
-    private double srcLat, srcLon, destLat, destLon, picLat, picLon;
+    private double srcLat, srcLon, destLat, destLon;
+    //private double picLat, picLon;
 
-    public void getDirection(double dirLat, double dirLon) {
-        double adjustedDirLat = dirLat - srcLat;
-        double adjustedDirLon = dirLon - srcLon;
-        String LatDirection;
-        String LonDirection;
-        String DirLatDirection;
-        String DirLonDirection;
-        if (picLon > 0) {
-            LonDirection = "east";
-        } else {
-            LonDirection = "west";
-        }
-        if (picLat > 0) {
-            LatDirection = "north";
-        } else {
-            LatDirection = "south";
-        }
-        if (dirLon - srcLon >= 0) {
-            DirLonDirection = "east";
-        } else {
-            DirLonDirection = "west";
-        }
-        if (dirLat - srcLat >= 0) {
-            DirLatDirection = "north";
-        } else {
-            DirLatDirection = "south";
-        }
-        System.out.println("Looking towards "+(picLon+ srcLon)+", "+(picLat+ srcLat)+"\nShould look at "+dirLon+", "+dirLat+"\nSo, turn "+getAngleAlt(adjustedDirLon,adjustedDirLat));
-        System.out.println("-------------------------------------");
-    }
+//    public void getDirection(double dirLat, double dirLon) {
+//        double adjustedDirLat = dirLat - srcLat;
+//        double adjustedDirLon = dirLon - srcLon;
+//        String LatDirection;
+//        String LonDirection;
+//        String DirLatDirection;
+//        String DirLonDirection;
+//        if (picLon > 0) {
+//            LonDirection = "east";
+//        } else {
+//            LonDirection = "west";
+//        }
+//        if (picLat > 0) {
+//            LatDirection = "north";
+//        } else {
+//            LatDirection = "south";
+//        }
+//        if (dirLon - srcLon >= 0) {
+//            DirLonDirection = "east";
+//        } else {
+//            DirLonDirection = "west";
+//        }
+//        if (dirLat - srcLat >= 0) {
+//            DirLatDirection = "north";
+//        } else {
+//            DirLatDirection = "south";
+//        }
+//        System.out.println("Looking towards "+(picLon+ srcLon)+", "+(picLat+ srcLat)+"\nShould look at "+dirLon+", "+dirLat+"\nSo, turn "+getAngleAlt(adjustedDirLon,adjustedDirLat));
+//        System.out.println("-------------------------------------");
+//    }
 
     public double getAngle(double dirLon, double dirLat, double picLat, double picLon) {
         double dotProduct = dotProduct(picLon, picLat,dirLon,dirLat);
@@ -58,14 +59,14 @@ public class Nav {
     private double magnitude(double x, double y){
         return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
     }
-    private double getAngleAlt(double x, double y){
-        double angle =  Math.toDegrees(Math.atan2(y - picLat, x - picLon));
-
-//        if(angle < 0){
-//            angle += 360;
-//        }
-
-        return angle;
-    }
+//    private double getAngleAlt(double x, double y){
+//        double angle =  Math.toDegrees(Math.atan2(y - picLat, x - picLon));
+//
+////        if(angle < 0){
+////            angle += 360;
+////        }
+//
+//        return angle;
+//    }
 }
 
