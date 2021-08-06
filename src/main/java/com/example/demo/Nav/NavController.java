1783b963895d961e6150b5832054b2d736010908
VasileosK <vasileios@konstantinou.dev>
2021-08-06 16:45:05 +0300

    add distance endpoint

--- src/main/java/com/example/demo/Nav/NavController.java
+++ src/main/java/com/example/demo/Nav/NavController.java
@@ -1,11 +1,19 @@
 package com.example.demo.Nav;
 
 import com.example.demo.Instruction.Instruction;
+import com.squareup.okhttp.RequestBody;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import com.squareup.okhttp.*;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+import java.io.File;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,8 +22,32 @@ public class NavController {
 
     private final NavService service;
 
-    @PostMapping("")
-    public Instruction getInstructionsFromPoint(@RequestBody Nav coords) {
-        return service.getInstructions(coords);
+    //@PostMapping("")
+    //public Instruction getInstructionsFromPoint(@RequestBody Nav coords) {
+        // return service.getInstructions(coords);
+    //}
+
+    @PostMapping("/distance")
+    public String home(@RequestParam("image") MultipartFile file) throws IOException {
+
+        RequestBody body = new MultipartBuilder()
+                .addFormDataPart("image", file.getName()+ ".jpg", RequestBody.create(MediaType.parse("media/type"), multipartToFile(file,"image.jpg")))
+                .type(MultipartBuilder.FORM)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://localhost:5000")
+                .post(body)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public  static File multipartToFile(MultipartFile multipart, String fileName) throws IllegalStateException, IOException {
+        File convFile = new File(System.getProperty("java.io.tmpdir")+"/"+fileName);
+        multipart.transferTo(convFile);
+        return convFile;
     }
 }
--- src/main/java/com/example/demo/home/HomeController.java
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -1,7 +1,15 @@
 package com.example.demo.home;
 
+import com.squareup.okhttp.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
 
 @Controller
 public class HomeController {
@@ -9,4 +17,5 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
 }
