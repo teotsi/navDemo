40479a22ad0aeffb82ccc17a3a914c9b666f6833
VasileosK <vasileios@konstantinou.dev>
2021-08-09 13:17:50 +0300

    clean up imports

--- src/main/java/com/example/demo/Nav/NavController.java
+++ src/main/java/com/example/demo/Nav/NavController.java
@@ -2,9 +2,10 @@ package com.example.demo.Nav;
 
 import com.example.demo.Instruction.Instruction;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
--- src/main/java/com/example/demo/home/HomeController.java
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -1,14 +1,7 @@
 package com.example.demo.home;
 
-import com.squareup.okhttp.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
 
 
 @Controller
